ca549ff1ab561f102ebe278efcca6e8acdfda754
Ashwin <ashwinstalin2002@gmail.com>
2026-03-08 22:47:13 +0530

    - add ros2 pkg - gazebo simulation completed - lidar scan visualize in rviz2

---
+++ ros2_pkg/vora_robot/launch/gz_sim.launch.py
@@ -0,0 +1,73 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+import xacro
+
+def generate_launch_description():
+    robotXacroName='robot'
+    namePackage='vora_robot'
+    modelFilRelativePath='urdf/mobile_robot.xacro'
+    pathModelFile = os.path.join(get_package_share_directory(namePackage), modelFilRelativePath)
+    robotDescription = xacro.process_file(pathModelFile).toxml()
+    gazebo_rosPackageLaunch = PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'))
+    gazeboLaunch = IncludeLaunchDescription(
+        gazebo_rosPackageLaunch,
+        launch_arguments={
+            "gz_args" : '-r -v 4 empty.sdf',
+            "on_exit_shutdown": 'true'
+        }.items()
+    )
+
+    spawnRobotNode = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            "-topic", "robot_description",
+            "-name", "robot",
+            "-allow_renaming", "false",
+            "-z", "0.32",
+            "-x", "0.0",
+            "-y", "0.0",
+            "-Y", "0.0"
+        ],            
+        output='screen',
+    )
+
+    robotStatePublisherNode = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='both',
+        parameters=[{'robot_description': robotDescription, 'use_sim_time': True}],
+    )
+
+    bridge_params = os.path.join(
+        get_package_share_directory(namePackage),
+        'config',
+        'bridge_parameters.yaml'
+    )
+
+    # Bridge ROS topics and Gazebo messages for establishing communication
+    start_gazebo_ros_bridge_cmd = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}',
+        ],
+        parameters=[{'use_sim_time': True}],
+        output='screen'
+      )
+    
+    launchDescriptionObject = LaunchDescription()
+    launchDescriptionObject.add_action(gazeboLaunch)
+    launchDescriptionObject.add_action(spawnRobotNode)
+    launchDescriptionObject.add_action(robotStatePublisherNode)
+    launchDescriptionObject.add_action(start_gazebo_ros_bridge_cmd)
+
+    return launchDescriptionObject
---
+++ ros2_pkg/vora_robot/launch/rviz.launch.py
@@ -0,0 +1,26 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    package_name = 'vora_robot'
+    
+    rviz_config_path = os.path.join(
+        get_package_share_directory(package_name),
+        'config',
+        'rviz_config.rviz'
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config_path],
+        parameters=[{'use_sim_time': True}]
+    )
+
+    return LaunchDescription([
+        rviz_node
+    ])
